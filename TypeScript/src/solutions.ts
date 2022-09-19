8bf8ed9ecd4be81163594f4ed798b26a7d4db4b1
John Solly <jsolly@protonmail.com>
2022-09-18 21:47:47 -0700

    Add solution for Sum of 1d Array

--- TypeScript/src/solutions.ts
+++ TypeScript/src/solutions.ts
@@ -1,4 +1,16 @@
 export class Kata {
+    /*A Tidy number is a number whose digits are in non-decreasing order.*/
+    static tidyNumber(num: number): boolean {
+        const numSplitToString = num.toString().split("");
+        const numArray = numSplitToString.map(Number);
+        for(let i = 0; i < numArray.length; i++) {
+            if(numArray[i] > numArray[i+1]) {
+                return false;
+            }
+        }
+        return true
+    }
+
     /* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.*/
     static squareDigits(num: number): number {
         const numSplitToString = num.toString().split("");
@@ -10,10 +22,15 @@ export class Kata {
         return squaredArrayToStringJoinedToNumber;
     }
 
-    static squareDigits2(num: number) {
-        return +num.toString()
-                   .split('')
-                   .map(n => Math.pow(+n,2))
-                   .join('');
-      }
+    /* Running Sum of 1d Array
+    Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+    Return the running sum of nums.
+    */
+   static runningSum(nums: number[]): number[] {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] += nums[i - 1]
+    }
+    return nums
+
   }
+}
--- TypeScript/tests/solutions.test.ts
+++ TypeScript/tests/solutions.test.ts
@@ -1,7 +1,29 @@
 import { expect, test } from 'vitest';
 import { Kata } from '../src/solutions';
 
-test.concurrent('Sum', () => {
+test.concurrent('Square Digets', () => {
     const result = Kata.squareDigits(9119);
     expect(result).toBe(811181);
   });
+
+
+test.concurrent('Tidy Number True', () => {
+    const result = Kata.tidyNumber(12);
+    expect(result).toBe(true);
+  });
+
+test.concurrent('Tidy Number False', () => {
+    const result = Kata.tidyNumber(32);
+    expect(result).toBe(false);
+  });
+
+
+test.concurrent('Running Sum 1', () => {
+    const result = Kata.runningSum([1,2,3,4]);
+    expect(result).toEqual([1,3,6,10]);
+  });
+
+test.concurrent('Running Sum 2', () => {  
+    const result = Kata.runningSum([1,1,1,1,1]);
+    expect(result).toEqual([1,2,3,4,5]);
+  });  
