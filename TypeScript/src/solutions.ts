4ce9ab26035e998beed5529eca57a2a81aa11592
John Solly <jsolly@protonmail.com>
2022-09-18 22:52:07 -0700

    Add running sum

--- TypeScript/src/solutions.ts
+++ TypeScript/src/solutions.ts
@@ -1,36 +1,56 @@
 export class Kata {
-    /*A Tidy number is a number whose digits are in non-decreasing order.*/
-    static tidyNumber(num: number): boolean {
-        const numSplitToString = num.toString().split("");
-        const numArray = numSplitToString.map(Number);
-        for(let i = 0; i < numArray.length; i++) {
-            if(numArray[i] > numArray[i+1]) {
-                return false;
-            }
-        }
-        return true
+  /*A Tidy number is a number whose digits are in non-decreasing order.*/
+  static tidyNumber(num: number): boolean {
+    const numSplitToString = num.toString().split('');
+    const numArray = numSplitToString.map(Number);
+    for (let i = 0; i < numArray.length; i++) {
+      if (numArray[i] > numArray[i + 1]) {
+        return false;
+      }
     }
+    return true;
+  }
 
-    /* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.*/
-    static squareDigits(num: number): number {
-        const numSplitToString = num.toString().split("");
-        const numArray = numSplitToString.map(Number);
-        const squaredArray = numArray.map((num) => num * num);
-        const squaredArrayToString = squaredArray.map(String);
-        const squaredArrayToStringJoined = squaredArrayToString.join("");
-        const squaredArrayToStringJoinedToNumber = Number(squaredArrayToStringJoined);
-        return squaredArrayToStringJoinedToNumber;
-    }
+  /* Welcome. In this kata, you are asked to square every digit of a number and concatenate them.*/
+  static squareDigits(num: number): number {
+    const numSplitToString = num.toString().split('');
+    const numArray = numSplitToString.map(Number);
+    const squaredArray = numArray.map((num) => num * num);
+    const squaredArrayToString = squaredArray.map(String);
+    const squaredArrayToStringJoined = squaredArrayToString.join('');
+    const squaredArrayToStringJoinedToNumber = Number(
+      squaredArrayToStringJoined,
+    );
+    return squaredArrayToStringJoinedToNumber;
+  }
 
-    /* Running Sum of 1d Array
+  /* Running Sum of 1d Array
     Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
     Return the running sum of nums.
     */
-   static runningSum(nums: number[]): number[] {
-    for(let i = 1; i < nums.length; i++) {
-        nums[i] += nums[i - 1]
+  static runningSum(nums: number[]): number[] {
+    for (let i = 1; i < nums.length; i++) {
+      nums[i] += nums[i - 1];
     }
-    return nums
+    return nums;
+  }
 
+  /* Find Pivot Index
+    Given an array of integers nums, write a method that returns the "pivot" index of this array.
+    */
+  static pivotIndex(nums: number[]): number {
+    function mySum(arr: number[]) {
+      return arr.reduce((a, b) => a + b, 0);
+    }
+
+    let leftSum = 0;
+    const SUM = mySum(nums);
+    for (let i = 0; i < nums.length; i++) {
+      if (leftSum == SUM - leftSum - nums[i]) {
+        return i;
+      }
+      leftSum += nums[i];
+    }
+    return -1;
   }
 }
--- TypeScript/tests/solutions.test.ts
+++ TypeScript/tests/solutions.test.ts
@@ -2,28 +2,41 @@ import { expect, test } from 'vitest';
 import { Kata } from '../src/solutions';
 
 test.concurrent('Square Digets', () => {
-    const result = Kata.squareDigits(9119);
-    expect(result).toBe(811181);
-  });
-
+  const result = Kata.squareDigits(9119);
+  expect(result).toBe(811181);
+});
 
 test.concurrent('Tidy Number True', () => {
-    const result = Kata.tidyNumber(12);
-    expect(result).toBe(true);
-  });
+  const result = Kata.tidyNumber(12);
+  expect(result).toBe(true);
+});
 
 test.concurrent('Tidy Number False', () => {
-    const result = Kata.tidyNumber(32);
-    expect(result).toBe(false);
-  });
-
+  const result = Kata.tidyNumber(32);
+  expect(result).toBe(false);
+});
 
 test.concurrent('Running Sum 1', () => {
-    const result = Kata.runningSum([1,2,3,4]);
-    expect(result).toEqual([1,3,6,10]);
-  });
+  const result = Kata.runningSum([1, 2, 3, 4]);
+  expect(result).toEqual([1, 3, 6, 10]);
+});
 
-test.concurrent('Running Sum 2', () => {  
-    const result = Kata.runningSum([1,1,1,1,1]);
-    expect(result).toEqual([1,2,3,4,5]);
-  });  
+test.concurrent('Running Sum 2', () => {
+  const result = Kata.runningSum([1, 1, 1, 1, 1]);
+  expect(result).toEqual([1, 2, 3, 4, 5]);
+});
+
+test.concurrent('Find Pivot Index', () => {
+  const result = Kata.pivotIndex([1, 7, 3, 6, 5, 6]);
+  expect(result).toBe(3);
+});
+
+test.concurrent('Find Pivot Index 2', () => {
+  const result = Kata.pivotIndex([1, 2, 3]);
+  expect(result).toBe(-1);
+});
+
+test.concurrent('Find Pivot Index 3', () => {
+  const result = Kata.pivotIndex([2, 1, -1]);
+  expect(result).toBe(0);
+});
