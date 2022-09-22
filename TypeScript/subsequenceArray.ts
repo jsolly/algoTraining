260a2ce2fd098f39110e046795838c28ac78105d
John Solly <jsolly@protonmail.com>
2022-09-21 22:58:29 -0700

    Add algo for finding string substrings

---
+++ TypeScript/subsequenceArray.ts
@@ -0,0 +1,25 @@
+function getSubSequences(myString: string): string[][] {
+  const subSequences: string[][] = [];
+  helper(myString, 0, []);
+  return subSequences;
+
+  function helper(s: string, index: number, path: string[]): void {
+    // Print the subsequence when reaching the leaf of recursion tree
+    if (index == myString.length) {
+      // Condition to avoid printing empty subsequence
+      if (path.length > 0) {
+        const pathDeepCopy = JSON.parse(JSON.stringify(path)) as string[];
+        subSequences.push(pathDeepCopy);
+      }
+    } else {
+      // Subsequence without including the element at current index
+      helper(myString, index + 1, path);
+      path.push(myString[index]);
+      // Subsequence including the element at current index
+      helper(myString, index + 1, path);
+      path.pop();
+    }
+  }
+}
+
+console.log(getSubSequences('abc'));
